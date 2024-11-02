9896757e1185bca8adc38c40cbf56743cfa37ab1
Fire551 <grantaring@gmail.com>
2024-11-02 17:43:20 -0400

    fixed player, edgecases for moveblock, unityevent for flipblock

--- Assets/Scripts/FlipBlock.cs
+++ Assets/Scripts/FlipBlock.cs
@@ -1,28 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class FlipBlock : MonoBehaviour
 {
     public GameObject dayObj;
     public GameObject nightObj;
+
+
+    public UnityEvent swappedToDay = new UnityEvent();
+    public UnityEvent swappedToNight = new UnityEvent();
+
+
     // Start is called before the first frame update
     void Start()
     {
         PlayerMovement.swappedToDay.AddListener(SwapBlockDay);
         PlayerMovement.swappedToNight.AddListener(SwapBlockNight);
 
-        dayObj.SetActive(PlayerMovement.isDay);
-        nightObj.SetActive(!PlayerMovement.isDay);
+        if (dayObj) dayObj.SetActive(PlayerMovement.isDay);
+        if (nightObj) nightObj.SetActive(!PlayerMovement.isDay);
     }
     void SwapBlockDay()
     {
-        dayObj.SetActive(true);
-        nightObj.SetActive(false);
+        swappedToDay.Invoke();
+        if (dayObj) dayObj.SetActive(true);
+        if (nightObj) nightObj.SetActive(false);
     }
     void SwapBlockNight()
     {
-        dayObj.SetActive(false);
-        nightObj.SetActive(true);
+        swappedToNight.Invoke();
+        if (dayObj) dayObj.SetActive(false);
+        if (nightObj) nightObj.SetActive(true);
     }
 }
